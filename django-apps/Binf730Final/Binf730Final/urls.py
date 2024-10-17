fe589fbbf65ee7f4a196854c84faa9ac29d7f388
cabralbj <brauliojcabrl@gmail.com>
2024-10-17 14:56:41 -0400

    Update to urls.py

--- django-apps/Binf730Final/Binf730Final/urls.py
+++ django-apps/Binf730Final/Binf730Final/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.upload_sequence_files, name='upload_sequence_files'),
-    path('method-and-score-scheme/', views.method_and_score_scheme, name='method_and_score_scheme'),
+    path('method_and_score_scheme/', views.method_and_score_scheme, name='method_and_score_scheme'),
     path('align/', views.align_sequences, name='align_sequences'),
 ]
