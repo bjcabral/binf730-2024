ebc2be0fc24e6260e55e41d815179d5e1f0d8597
brauliocabral <brauliojcabral@gmail.com>
2024-10-18 15:10:45 -0400

    Update to uploadFiles(), uploadForm, and upload.html

--- django-apps/Binf730Final/filesUpload/forms.py
+++ django-apps/Binf730Final/filesUpload/forms.py
@@ -7,14 +7,14 @@ class AlignmentScoreForm(forms.Form):
     mismatch_score = forms.IntegerField(label='Mismatch: ')
     gap_score = forms.IntegerField(label='Gap Score: ')
 
-    class SequenceFileForm(forms.Form):
-        SEQUENCE_INPUT_CHOICES = [
-            ('manual', 'Enter sequences manually'),
-            ('fasta', 'Upload FASTA files'),
-        ]
-        sequence_input_type = forms.ChoiceField(choices=SEQUENCE_INPUT_CHOICES, widget=forms.RadioSelect)
-        number_of_sequences = forms.IntegerField(min_value=1, required=False)
-        number_of_fasta_files = forms.IntegerField(min_value=1, required=False)
+class SequenceFileForm(forms.Form):
+    SEQUENCE_INPUT_CHOICES = [
+        ('manual', 'Enter sequences manually'),
+        ('fasta', 'Upload FASTA files'),
+    ]
+    sequence_input_type = forms.ChoiceField(choices=SEQUENCE_INPUT_CHOICES, widget=forms.RadioSelect)
+    number_of_sequences = forms.IntegerField(min_value=1, required=False)
+    number_of_fasta_files = forms.IntegerField(min_value=1, required=False)
 
 class AlignmentMethodForm(forms.Form):
     ALIGNMENT_CHOICES = [
