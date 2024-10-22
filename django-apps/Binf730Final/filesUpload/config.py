f12384383402d560e8e544e1aaf81d7cb7125a66
brauliocabral <brauliojcabral@gmail.com>
2024-10-22 13:14:02 -0400

    Another updte to aligne_sequences()

--- django-apps/Binf730Final/filesUpload/config.py
+++ django-apps/Binf730Final/filesUpload/config.py
@@ -2,4 +2,4 @@
 
 DATA_DIR = "/home/ubuntu/binf-730/django-apps/Binf730Final/media"
 ALIGNED_FILE_NAME = "aligned_file.aln"
-INPUT_FILE_NAME = "sequences.fasta"
+INPUT_FILE_NAME = "uploaded_sequences.fasta"
